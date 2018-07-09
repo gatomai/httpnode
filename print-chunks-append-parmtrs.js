b513be22626eb2d9bdcba513e7c3f97cd26def67
Sai Gautam <sai.gautam@gmail.com>
2018-07-09 17:01:12 +0000

    Added parameters to the function so far

---
+++ print-chunks-append-parmtrs.js
@@ -0,0 +1,36 @@
+var https = require('https');
+
+function getAndPrintHTML(options) {
+
+    var fullchunk = "";
+
+    https.get(requestOptions, function (response) {
+
+        // set encoding of received data to UTF-8
+        response.setEncoding('utf8');
+
+        // the callback is invoked when a `data` chunk is received
+        response.on('data', function (data) {
+            fullchunk = fullchunk + data;
+            // console.log('Data Received:', data);
+        console.log('Chunk Received. Length:', data.length);
+        });
+
+        // the callback is invoked when all of the data has been received
+        // (the `end` of the stream)
+        response.on('end', function () {
+            console.log('Response stream complete.');
+            console.log('This is ' + fullchunk);
+        });
+
+    });
+
+}
+
+var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step3.html'
+  };
+
+getAndPrintHTML(requestOptions);
+
